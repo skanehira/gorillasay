9a63eb7dcab67d81a9d920fd072ddf721f41215e
skanehira <sho19921005@gmail.com>
2020-03-02 18:27:49 +0900

    implement

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skanehira/gorillsay
+
+go 1.13
+
+require github.com/mattn/go-runewidth v0.0.8
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/mattn/go-runewidth"
+)
+
+const gorilla = `
+%s
+           \
+            \
+             \
+             ,.-" "-.,
+            /   ===   \
+           /  =======  \
+        __|  (o)   (0)  |__
+       / _|    .---.    |_ \
+      | /.----/ O O \----.\ |
+       \/     |     |     \/
+       |                   |
+       |                   |
+       |                   |
+       _\   -.,_____,.-   /_
+   ,.-"  "-.,_________,.-"  "-.,
+  /          |       |          \
+ |           l.     .l           |
+ |            |     |            |
+ l.           |     |           .l
+  |           l.   .l           | \,
+  l.           |   |           .l   \,
+   |           |   |           |      \,
+   l.          |   |          .l        |
+    |          |   |          |         |
+    |          |---|          |         |
+    |          |   |          |         |
+    /"-.,__,.-"\   /"-.,__,.-"\"-.,_,.-"\
+   |            \ /            |         |
+   |             |             |         |
+    \__|__|__|__/ \__|__|__|__/ \_|__|__/
+`
+
+func main() {
+	args := os.Args[1:]
+	var length int
+
+	for _, w := range args {
+		wl := runewidth.StringWidth(w)
+		if wl > length {
+			length = wl
+		}
+	}
+	length = length / 2
+
+	str := strings.Builder{}
+
+	for i := 0; i < length; i++ {
+		str.WriteString("🍌")
+	}
+
+	str.WriteString("\n")
+	str.WriteString(strings.Join(args, "\n"))
+	str.WriteString("\n")
+
+	for i := 0; i < length; i++ {
+		str.WriteString("🍌")
+	}
+
+	fmt.Printf(gorilla, str.String())
+}
